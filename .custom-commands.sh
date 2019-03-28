4fb703c961ea981392508aaa49d19933998f009d
adleypc <jadley08@gmail.com>
2019-03-28 18:58:54 -0400

    added comments

--- .custom-commands.sh
+++ .custom-commands.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+# Creates a new directory and changed current directory to inside
 mkcd () {
 	mkdir -p -- "$1" &&
 		cd -P -- "$1"
 }
 
+# Opens a new terminal in current working directory
 nt () {
 	pwd | urxvt
 }
