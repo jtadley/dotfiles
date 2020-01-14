bfe0ba82c5bfb735df3fed6613e9322c593e7f0d
adleypc <jadley08@gmail.com>
2020-01-14 14:26:45 -0500

    rough working 3-head setup

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -8,5 +8,7 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar, using default config location ~/.config/polybar/config
 polybar adley-bar &
+polybar adley-bar-external &
+polybar adley-bar-external-2 &
 
 echo "Polybar launched."
